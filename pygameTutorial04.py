ed078cf69a2eb1d4b8332407bd495331325711d8
Jim Veneskey <github@strifecrafter.com>
2014-12-20 08:36:56 -0500

    Adding new files to this new repo

---
+++ pygameTutorial04.py
@@ -0,0 +1,33 @@
+import pygame
+
+x = pygame.init()
+
+print(x)
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Slither')
+
+# update the entire surface
+pygame.display.flip()
+
+# update part of the surface (optionally) or the entire surface (default)
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+#        print(event)
+        if event.type == pygame.QUIT:
+            print 'Quit event detected, quitting game'
+            gameExit = True
+            
+
+    
+
+
+# unitialize
+pygame.quit()
+
+quit()
+
