dfd1068761eb58776f664f0d7ceaf1a1ec8dd2b4
Jim Veneskey <github@strifecrafter.com>
2014-12-20 13:08:56 -0500

    More updates

--- pygameTutorial12.py
+++ pygameTutorial12.py
@@ -21,6 +21,7 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+lead_y_change = 0
 clock = pygame.time.Clock()
 fps = 15
 
@@ -33,15 +34,33 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
+                lead_y_change = 0
+                
             elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
-        
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+                
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
                 lead_x_change = 0
                 
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
+                lead_x_change = 0
+        
+#        if event.type == pygame.KEYUP:
+#            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+#                lead_x_change = 0
+    
+    if lead_x >= 800 or lead_x <= 0 or lead_y >= 600 or lead_y <= 0:
+        print "You have left the game screen - the game is over!"
+        gameExit = True
+        
+    
                 
-    lead_x += lead_x_change        
+                
+    lead_x += lead_x_change
+    lead_y += lead_y_change        
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
     pygame.display.update()
