099cae83ce70563be8ae886db50485f9f4600439
Jim Veneskey <github@strifecrafter.com>
2015-01-09 13:55:46 -0500

    Pygame Tutorial #08

--- Slither.py
+++ Slither.py
@@ -18,18 +18,26 @@ pygame.display.flip()
 
 gameExit = False
 
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+
+
 while not gameExit:
     for event in pygame.event.get():
-#        print(event)
         if event.type == pygame.QUIT:
             print 'Quit event detected, quitting game'
             gameExit = True
-            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x_change = -10
+            elif event.key == pygame.K_RIGHT:
+                lead_x_change = 10
+                
+    lead_x += lead_x_change        
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [400,300,10,10])
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
 
-    gameDisplay.fill(red, rect = [200,200, 50, 50])
-    
     
     pygame.display.update()
     
