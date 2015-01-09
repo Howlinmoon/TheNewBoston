afcdfdf2b9f2cef7bb94f51b365048d918d18587
Jim Veneskey <github@strifecrafter.com>
2015-01-09 14:01:14 -0500

    Pygame Tutorial #13

--- Slither.py
+++ Slither.py
@@ -8,8 +8,13 @@ green = (0,255,0)
 blue = (0,0.255)
 black = (0,0,0)
 
+display_width = 800
+display_height = 600
+block_size = 10
+fps = 15
 
-gameDisplay = pygame.display.set_mode((800,600))
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Slither')
 
 # update the entire surface
@@ -18,12 +23,12 @@ pygame.display.flip()
 
 gameExit = False
 
-lead_x = 300
-lead_y = 300
+lead_x = display_width / 2
+lead_y = display_height / 2
+
 lead_x_change = 0
 lead_y_change = 0
 clock = pygame.time.Clock()
-fps = 15
 
 while not gameExit:
     for event in pygame.event.get():
@@ -33,26 +38,26 @@ while not gameExit:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -10
+                lead_x_change = -block_size
                 lead_y_change = 0
                 
             elif event.key == pygame.K_RIGHT:
-                lead_x_change = 10
+                lead_x_change = block_size
                 lead_y_change = 0
                 
             elif event.key == pygame.K_UP:
-                lead_y_change = -10
+                lead_y_change = -block_size
                 lead_x_change = 0
                 
             elif event.key == pygame.K_DOWN:
-                lead_y_change = 10
+                lead_y_change = block_size
                 lead_x_change = 0
         
 #        if event.type == pygame.KEYUP:
 #            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 #                lead_x_change = 0
     
-    if lead_x >= 800 or lead_x <= 0 or lead_y >= 600 or lead_y <= 0:
+    if lead_x >= display_width or lead_x <= 0 or lead_y >= display_height or lead_y <= 0:
         print "You have left the game screen - the game is over!"
         gameExit = True
         
@@ -62,7 +67,7 @@ while not gameExit:
     lead_x += lead_x_change
     lead_y += lead_y_change        
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,block_size,block_size])
     pygame.display.update()
     
     clock.tick(fps)
