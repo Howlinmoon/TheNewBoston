35a41960d3835d23c4b93ee91d10dd1ece1cf110
Jim Veneskey <github@strifecrafter.com>
2015-01-09 14:16:24 -0500

    Pygame Tutorial #31

--- Slither.py
+++ Slither.py
@@ -55,9 +55,9 @@ def text_objects(text, color):
     return textSurface, textSurface.get_rect()
 
 # displaying the text object
-def message_to_screen(msg, color):
+def message_to_screen(msg, color, y_displace=0):
     textSurf,textRect = text_objects(msg,color)
-    textRect.center = (display_width/2), (display_height/2)
+    textRect.center = (display_width/2), (display_height/2)+y_displace
     gameDisplay.blit(textSurf, textRect)
     pygame.display.update()
 
@@ -86,7 +86,8 @@ def gameLoop():
         
         while gameOver == True:
             gameDisplay.fill(white)
-            message_to_screen("Game Over, press C to play again, or Q to quit", red)
+            message_to_screen("Press C to play again, or Q to quit", black, 50)
+            message_to_screen("Game Over", red, -50)
             pygame.display.update()
             for event in pygame.event.get():
                 
