6152379eca55912ec0bb78bb1a1439e9e7d4438d
Jim Veneskey <github@strifecrafter.com>
2015-01-09 14:07:32 -0500

    Pygame Tutorial #21

--- Slither.py
+++ Slither.py
@@ -45,7 +45,7 @@ def gameLoop():
     lead_y_change = 0
 
     snakeList = []
-    snakeLength = 5
+    snakeLength = 1
     
     
     randAppleX = round(random.randrange(0, display_width - block_size)/ 10.0) * 10.0
@@ -116,12 +116,21 @@ def gameLoop():
         if len(snakeList) > snakeLength:
             del snakeList[0]
         
+        # snake collision detection
+        for eachSegment in snakeList[:-1]:
+            if eachSegment == snakeHead:
+                # collision detected
+                print "Snake hit itself!"
+                gameOver = True
+        
+        
         snake(block_size, snakeList)
         pygame.display.update()
         
         if lead_x == randAppleX and lead_y == randAppleY:
             randAppleX = round(random.randrange(0, display_width - block_size)/ 10.0) * 10.0
             randAppleY = round(random.randrange(0, display_height - block_size)/ 10.0) * 10.0
+            snakeLength +=1
         
         clock.tick(fps)
         
