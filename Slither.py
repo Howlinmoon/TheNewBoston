128702d7d81ba6ac7bcf0c25798650d5cdaeec3c
Jim Veneskey <github@strifecrafter.com>
2015-01-09 14:03:45 -0500

    Pygame Tutorial #15

--- Slither.py
+++ Slither.py
@@ -13,8 +13,12 @@ display_height = 600
 block_size = 10
 fps = 15
 
-#font = pygame.font.SysFont(None, 25)
-font = pygame.font.Font(None, 20)
+font = pygame.font.SysFont(None, 25)
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Slither')
+
+# update the entire surface
+pygame.display.flip()
 
 def message_to_screen(msg, color):
     screen_text = font.render(msg, True, color)
@@ -22,71 +26,80 @@ def message_to_screen(msg, color):
     pygame.display.update()
 
 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Slither')
+def gameLoop():
+    
+    gameExit = False
+    gameOver = False
+    
+    lead_x = display_width / 2
+    lead_y = display_height / 2
+    
+    lead_x_change = 0
+    lead_y_change = 0
+    clock = pygame.time.Clock()
 
-# update the entire surface
-pygame.display.flip()
-
-
-gameExit = False
-
-lead_x = display_width / 2
-lead_y = display_height / 2
-
-lead_x_change = 0
-lead_y_change = 0
-clock = pygame.time.Clock()
-
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print 'Quit event detected, quitting game'
-            gameExit = True
+    while not gameExit:
         
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                lead_x_change = -block_size
-                lead_y_change = 0
-                
-            elif event.key == pygame.K_RIGHT:
-                lead_x_change = block_size
-                lead_y_change = 0
-                
-            elif event.key == pygame.K_UP:
-                lead_y_change = -block_size
-                lead_x_change = 0
-                
-            elif event.key == pygame.K_DOWN:
-                lead_y_change = block_size
-                lead_x_change = 0
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("Game Over, press C to play again, or Q to quit", red)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+                        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print 'Quit event detected, quitting game'
+                gameExit = True
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change = -block_size
+                    lead_y_change = 0
+                    
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                    
+                elif event.key == pygame.K_UP:
+                    lead_y_change = -block_size
+                    lead_x_change = 0
+                    
+                elif event.key == pygame.K_DOWN:
+                    lead_y_change = block_size
+                    lead_x_change = 0
+            
+    #        if event.type == pygame.KEYUP:
+    #            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+    #                lead_x_change = 0
         
-#        if event.type == pygame.KEYUP:
-#            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-#                lead_x_change = 0
-    
-    if lead_x >= display_width or lead_x <= 0 or lead_y >= display_height or lead_y <= 0:
-        print "You have left the game screen - the game is over!"
-        gameExit = True
+        if lead_x >= display_width or lead_x <= 0 or lead_y >= display_height or lead_y <= 0:
+            print "You have left the game screen - the game is over!"
+            gameOver = True
+            
+        
+                    
+                    
+        lead_x += lead_x_change
+        lead_y += lead_y_change        
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,block_size,block_size])
+        pygame.display.update()
+        
+        clock.tick(fps)
+        
         
     
-                
-                
-    lead_x += lead_x_change
-    lead_y += lead_y_change        
-    gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,block_size,block_size])
-    pygame.display.update()
+    # unitialize
+    pygame.quit()
     
-    clock.tick(fps)
-    
-    
+    quit()
 
-# unitialize
-message_to_screen("You Lost", red)
-time.sleep(2)
-pygame.quit()
-
-quit()
+gameLoop()
 
 
