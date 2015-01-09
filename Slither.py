b62d65e58f72195bb842de75124bccca2c692680
Jim Veneskey <github@strifecrafter.com>
2015-01-09 14:09:06 -0500

    Pygame Tutorial #25

--- Slither.py
+++ Slither.py
@@ -143,8 +143,10 @@ def gameLoop():
 #                 randAppleY = round(random.randrange(0, display_height - block_size)) #/ 10.0) * 10.0
 #                 snakeLength +=1
             
-        if lead_x > randAppleX and lead_x < randAppleX+AppleThickness:
-            
+        if (lead_x > randAppleX and lead_x < randAppleX+AppleThickness) or (lead_x + block_size > randAppleX and lead_x + block_size < randAppleX + AppleThickness):
+            # print ("x crossover detected")
+            if (lead_y > randAppleY and lead_y < randAppleY+AppleThickness) or (lead_y + block_size > randAppleY and lead_y + block_size < randAppleY + AppleThickness):
+                print("x and y crossover detected!")
         
         clock.tick(fps)
         
