92c78daa31d904de52f0e6cf90b7c84f5753856c
Jim Veneskey <github@strifecrafter.com>
2015-01-10 21:09:58 -0500

    PyGame Tutorial #94, still inching our way towards displaying a cube....

--- OpenGLDemo.py
+++ OpenGLDemo.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-# PyGame Tutorial #93
+# PyGame Tutorial #94
 
 verticies = (
     (1, -1, -1),         
@@ -38,3 +38,11 @@ def Draw_Cube ():
         for vertex in edge:
             glVertex3fv(verticies[vertex])
     glEnd()
+    
+    
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    
+    gluPerspective(45.0, (display[0]/display[1]), 1, 50.0)
