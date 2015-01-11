05f546ab123d6b00cbb55768026d529c53615919
Jim Veneskey <github@strifecrafter.com>
2015-01-10 20:55:02 -0500

    Pygame Tutorial #92 - Defining verticies and lines for the OpenGL Cube

--- OpenGLDemo.py
+++ OpenGLDemo.py
@@ -3,6 +3,31 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-# PyGame Tutorial #91
+# PyGame Tutorial #92
 
+verticies = (
+    (1, -1, -1),         
+    (1, 1, -1),         
+    (-1, 1, -1),         
+    (-1, -1, -1),         
+    (1, -1, 1),         
+    (1, 1, 1),         
+    (-1, -1, 1),         
+    (-1, 1, 1)         
+ )
+
+edge = (
+    (0,1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
 
