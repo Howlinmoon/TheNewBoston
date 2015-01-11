548b3e55ea84c6fa5cde0c2b6419f411dbed014d
Jim Veneskey <github@strifecrafter.com>
2015-01-10 21:18:20 -0500

    Pygame Tutorial #95 - finally get to see a rough cube - corrected obvious errors from previous updates

--- OpenGLDemo.py
+++ OpenGLDemo.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-# PyGame Tutorial #94
+# PyGame Tutorial #95
 
 verticies = (
     (1, -1, -1),         
@@ -16,7 +16,7 @@ verticies = (
     (-1, 1, 1)         
  )
 
-edge = (
+edges = (
     (0,1),
     (0, 3),
     (0, 4),
@@ -46,3 +46,21 @@ def main():
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     
     gluPerspective(45.0, (display[0]/display[1]), 1, 50.0)
+    
+    glTranslatef(0.0, 0.0, -5.0)
+    
+    glRotatef(20, 0, 0, 0)
+    
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+    
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Draw_Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+        
+main()
